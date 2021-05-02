6cd0efd4c3a49d241b430c04f6de9c1c795cda85
BenchiaRazvan <79082384+BenchiaRazvan@users.noreply.github.com>
2021-05-02 10:59:54 +0300

    Delete Optionallab9 directory

--- Optionallab9/src/main/java/com/lab8/EntityManager/AbstractRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.lab8.EntityManager;
-
-import com.lab8.entity.GenresEntity;
-
-public interface AbstractRepositoryInterface<T>{
-    void create(T entity);
-    void findById(T entity);
-    void findByName(T entity);
-}
